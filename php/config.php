7cd849af899f4ada41dbb9538e51588e80445ba4
ahmed badawi <ahmedbad063@gmail.com>
2024-04-27 00:25:07 +0300

    initial commit

---
+++ php/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$localhost="viaduct.proxy.rlwy.net";
+$username="root";
+$password="rJcRBgijbkpBLJZvFAAwlBIUhhPQJLfD";
+$database="railway";
+
+$conn=mysqli_connect($localhost,$username,$password,$database);
+
+?>
